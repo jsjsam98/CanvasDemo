00d131f0bbfd6f85d46a3be2438f33bc6e37b9d9
jsjsam98 <68052051+jsjsam98@users.noreply.github.com>
2023-03-26 14:53:04 -0400

    brushSize

--- src/DrawingBoard.js
+++ src/DrawingBoard.js
@@ -4,6 +4,7 @@ const DrawingBoard = ({ color }) => {
 	const canvasRef = useRef(null);
 	const [isDrawing, setIsDrawing] = useState(false);
 	const [startCoords, setStartCoords] = useState({ x: 0, y: 0 });
+	const [brushSize, setBrushSize] = useState(1);
 
 	const handleMouseDown = (event) => {
 		setIsDrawing(true);
@@ -20,6 +21,7 @@ const DrawingBoard = ({ color }) => {
 
 		const canvas = canvasRef.current;
 		const ctx = canvas.getContext("2d");
+		ctx.lineWidth = brushSize;
 		ctx.strokeStyle = color;
 		ctx.beginPath();
 		ctx.moveTo(startCoords.x, startCoords.y);
@@ -40,8 +42,21 @@ const DrawingBoard = ({ color }) => {
 		const ctx = canvas.getContext("2d");
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 	};
+	const handleBrushSizeChange = (event) => {
+		setBrushSize(event.target.value);
+	};
+
 	return (
-		<>
+		<div style={{ margin: "0.5em" }} className="parent">
+			<input
+				type="range"
+				id="brushSize"
+				name="brushSize"
+				min="1"
+				max="10"
+				value={brushSize}
+				onChange={handleBrushSizeChange}
+			/>
 			<canvas
 				className="canvas"
 				ref={canvasRef}
@@ -59,7 +74,7 @@ const DrawingBoard = ({ color }) => {
 			>
 				Clear
 			</button>
-		</>
+		</div>
 	);
 };
 
